0ee5336bb7afb7fb721e962101642b49f7942dcf
Amet13 <admin@amet13.name>
2025-09-06 21:19:23 -0400

    revert: Broken release

--- Sources/SharedUtils/AppConstants.swift
+++ Sources/SharedUtils/AppConstants.swift
@@ -5,7 +5,7 @@ public enum AppConstants {
   // MARK: - App Information
 
   public static let appName = "ODYSSEY"
-  public static let appVersion = "2.5.1"
+  public static let appVersion = "2.5.0"
   public static let appBundleId = "com.odyssey.app"
 
   // MARK: - Timeouts and Delays
