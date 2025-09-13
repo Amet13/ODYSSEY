9643572d40d5746b577434af88a78f6eeace2021
Amet13 <admin@amet13.name>
2025-09-12 21:18:20 -0400

    🔖 Release v2.6.1
    - Updated version to 2.6.1 in all files
    - Built and tested application

--- Sources/SharedUtils/AppConstants.swift
+++ Sources/SharedUtils/AppConstants.swift
@@ -5,7 +5,7 @@ public enum AppConstants {
   // MARK: - App Information
 
   public static let appName = "ODYSSEY"
-  public static let appVersion = "2.6.0"
+  public static let appVersion = "2.6.1"
   public static let appBundleId = "com.odyssey.app"
 
   // MARK: - Timeouts and Delays
